bcfcd273a7e59d706e69c6d3866780c11616c275
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-07-03 17:18:08 +0900

    Update default.js

--- .gitignore/js/default.js
+++ .gitignore/js/default.js
@@ -9,4 +9,10 @@ var min = Math.floor((time / 1000 / 60) % 60);
 var now =new Date();
 var target = new Date(2020,7,24,0,0,0,0);
 var diff = target.getTime() - now.getTime
-
+var counter = separate_time(diff);
+document.getElementById('countdoun').textContet = 
+  '東京オリンピックまであと   '　+
+  counter[3] + '日'　+
+  counter[2] + '時間'　+
+  counter[1] + '分'　+
+  counter[0] + '秒';
