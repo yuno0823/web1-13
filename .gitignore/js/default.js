a1194e291a1b25b701a04376601e2c585af599bd
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-07-03 17:15:16 +0900

    Update default.js

--- .gitignore/js/default.js
+++ .gitignore/js/default.js
@@ -1,1 +1,12 @@
+var separate_time = function(time){
+var sec = Math.floor((time / 1000) % 60);
+var min = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days = Math.floor( time / 1000 / 60 / 60 / 24);
+  return [sec, min, hours, days];
+}
+
+var now =new Date();
+var target = new Date(2020,7,24,0,0,0,0);
+var diff = target.getTime() - now.getTime
 
