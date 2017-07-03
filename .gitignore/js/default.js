464319974926257a180519b8dbf2633855324b3f
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-07-03 17:20:15 +0900

    Update default.js

--- .gitignore/js/default.js
+++ .gitignore/js/default.js
@@ -6,6 +6,7 @@ var min = Math.floor((time / 1000 / 60) % 60);
   return [sec, min, hours, days];
 }
 
+var update = function(){
 var now =new Date();
 var target = new Date(2020,7,24,0,0,0,0);
 var diff = target.getTime() - now.getTime
@@ -16,3 +17,10 @@ document.getElementById('countdoun').textContet =
   counter[2] + '時間'　+
   counter[1] + '分'　+
   counter[0] + '秒';
+refresh();
+}
+
+var refresh= function{
+setTimeout(update, 1000);
+}
+update();
