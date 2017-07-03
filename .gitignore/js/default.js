0027688b540230a6b78cba8549b5a2990b684099
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-07-03 17:58:48 +0900

    Update default.js

--- .gitignore/js/default.js
+++ .gitignore/js/default.js
@@ -45,3 +45,49 @@ opx.selected = true;
 document.getElementById('form').select.onchange = function(){
   location.href = document.getElementById('form').select.value;
 }
+
+function setCookie(c_name,value,expiredays){
+var extime = new Date().getTime();
+var cltime = new Date(extime + (60*60*24*1000*expiredays));
+var exdate = cltime.toUTCString();
+  var s="";
+  s += c_name +"="+ escape(value);
+  s += "; path="  location.pathname;
+  if(expiredays){
+  s += "; expires="  +exdate+"; ";
+  }else{
+  s += "; ";
+  }
+  document.cookie=s;
+}
+
+function getCookie(c_name){
+var st="";
+var ed="";
+if(0 < document.cookie.length)
+{st=document,cookie.indexOf(c_name + =");
+ if(st!=-1){
+st=st+c_name.length+1;
+ed=document.cookie.indexOf(";",st);
+if(ed==-1) ed=document.cookie.length;
+return unescape(document.cookie.substring(st,ed));
+}
+}
+  return "";
+}
+
+var last_date = getCookie('lastDate');
+if(last_date){
+document.getElementById('cookie').textContet = '前回訪れた時間: ' + last_date;
+}else{
+document.getElementById('cookie').textContet = '初めまして';
+}
+
+var current_time = new Date();
+setCookie('lastDate', current_time.toString(), 7);
+
+document.getElementById('remove_cookie').onsubmit = function(){
+setCooki('lastDate', "", 0);
+};
+
+}
